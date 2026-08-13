cffce7f80280f361c6d941e92108a2ddae55021d
Evgeny Stepanischev <imbolk@gmail.com>
2026-08-13 18:04:50 +0300

    Replace admin lock with pi mark

--- _include/src/Template/HtmlTemplate.php
+++ _include/src/Template/HtmlTemplate.php
@@ -349,7 +349,7 @@ class HtmlTemplate
             '<span class="engine-credit">' . $engineCredit . '</span>' .
             '<a class="visual-login" href="' . $this->urlBuilder->link('/_admin/index.php') .
             '" aria-label="' . $loginLabel . '" title="' . $loginLabel . '">' .
-            '<span aria-hidden="true">🔒</span></a>';
+            '<span aria-hidden="true">π</span></a>';
     }
 
     private function hasContent(string $placeholder): bool
