cecc59a550e8ea292099476227273705bd481a20
Evgeny Stepanischev <imbolk@gmail.com>
2026-08-13 18:07:20 +0300

    Use a blackletter R for the admin link

--- _include/src/Template/HtmlTemplate.php
+++ _include/src/Template/HtmlTemplate.php
@@ -349,7 +349,7 @@ class HtmlTemplate
             '<span class="engine-credit">' . $engineCredit . '</span>' .
             '<a class="visual-login" href="' . $this->urlBuilder->link('/_admin/index.php') .
             '" aria-label="' . $loginLabel . '" title="' . $loginLabel . '">' .
-            '<span aria-hidden="true">π</span></a>';
+            '<span aria-hidden="true">ℜ</span></a>';
     }
 
     private function hasContent(string $placeholder): bool
